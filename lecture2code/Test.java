9e38297808547e69069a336375e17ca583877508
Mehrdad Sabetzadeh <mehrdad.sabetzadeh@uni.lu>
2020-01-10 09:14:53 -0500

    no message

---
+++ lecture2code/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+	public static void main(String[] args) {
+		
+		/* Try uncommenting the curly brackets and see what happens. */
+		//{ 
+		int i = 5, j = i + 5; 
+		//}
+		
+		System.out.println(j);
+
+	}
+}
